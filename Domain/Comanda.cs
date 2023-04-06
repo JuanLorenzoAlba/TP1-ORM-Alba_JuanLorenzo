ebf5881228b9f0b09abb37e15cf73343106e9908
JuanLorenzoAlba <albajual@gmail.com>
2023-04-05 23:54:57 -0300

    clean architecture

---
+++ Domain/Comanda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Comanda
+    {
+        public Guid ComandaId { set; get; }
+        public int PrecioTotal { set; get; }
+        public DateTime Fecha { set; get; }
+
+        public int FormaEntregaId { get; set; }
+        public FormaEntrega FormaEntrega { set; get; }
+
+        public ICollection<ComandaMercaderia> ComandasMercaderias { get; set; }
+    }
+}
---
+++ Domain/TipoMercaderia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class TipoMercaderia
+    {
+        public int TipoMercaderiaId { get; set; }
+        public string Descripcion { get; set; }
+
+        public ICollection<Mercaderia> Mercaderias { get; set; }
+    }
+}
